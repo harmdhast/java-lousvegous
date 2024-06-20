719a4450c2afa5301c64cb94dc69227d6835c831
Harmdhast <pandemonium.harmdhast@gmail.com>
2024-06-21 01:31:41 +0200

    Passage en FXGL

--- src/main/java/fr/esgi/lousvegous/HelloApplication.java
+++ src/main/java/fr/esgi/lousvegous/HelloApplication.java
@@ -1,34 +1,61 @@
 package fr.esgi.lousvegous;
 
-import fr.esgi.lousvegous.symbol.SymbolManager;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.almasb.fxgl.app.CursorInfo;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.IntroScene;
+import com.almasb.fxgl.app.scene.SceneFactory;
+import com.almasb.fxgl.dsl.FXGL;
+import fr.esgi.lousvegous.ui.MyIntroScene;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.util.Objects;
-
-public class HelloApplication extends Application {
+public class HelloApplication extends GameApplication {
     public static void main(String[] args) {
-        //SymbolManager symbolManager = new SymbolManager();
+        //  SymbolManager symbolManager = new SymbolManager();
 
-//        Random random = new Random();
-//        for (int i = 0; i < 20; i++) {
-//            System.out.print(random.getRandomSymbol().getImage());
-//        }
+        //  Random random = new Random();
+        //  for (int i = 0; i < 20; i++) {
+        //      System.out.print(random.getRandomSymbol().getImage());
+        //  }
 
 
-        launch();
+        launch(args);
+    }
+
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("lousvegous.css")).toExternalForm());
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+    @Override
+    protected void initGame() {
+        var image = FXGL.getAssetLoader().loadTexture("test.jpg");
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("lousvegous.css")).toExternalForm());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(800);
+        gameSettings.setHeight(600);
+        gameSettings.setTitle("LousVegous");
+        gameSettings.setVersion("0.1");
+        gameSettings.setIntroEnabled(false);
+
+        CursorInfo cursorInfo = new CursorInfo("dollar.gif",0, 0);
+        gameSettings.setDefaultCursor(cursorInfo);
+        gameSettings.setIntroEnabled(true);
+        gameSettings.setSceneFactory(new SceneFactory() {
+            @NotNull
+            @Override
+            public IntroScene newIntro() {
+                return new MyIntroScene();
+            }
+        });
     }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module fr.esgi.javalousvegous {
+open module fr.esgi.javalousvegous {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -11,9 +11,11 @@ module fr.esgi.javalousvegous {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires java.desktop;
+    requires annotations;
 
-    opens fr.esgi.lousvegous to javafx.fxml;
+    //opens fr.esgi.lousvegous to javafx.fxml;
     exports fr.esgi.lousvegous;
     exports fr.esgi.lousvegous.symbol;
-    opens fr.esgi.lousvegous.symbol to javafx.fxml;
+    //opens fr.esgi.lousvegous.symbol to javafx.fxml;
+    //opens assets.textures;
 }
