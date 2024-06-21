1a6ebd3402cf389292403b9352a30e1eb5a51df9
KirahhY <marius.oudin@gmail.com>
2024-06-21 16:45:33 +0200

    Add Pattern & PatternManager

--- src/main/java/fr/esgi/lousvegous/pattern/Pattern.java
+++ src/main/java/fr/esgi/lousvegous/pattern/Pattern.java
@@ -1,9 +1,37 @@
 package fr.esgi.lousvegous.pattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pattern {
     String pattern;
+    int binary;
 
-    public Pattern(String pattern) {
-        this.pattern = pattern;
+// list avec les index de la position des 1
+
+    // Constructeur
+    Pattern(String pattern) {
+        this.pattern = pattern.replaceAll("\\s", ""); // supprime les espaces
+        binary = this.binaryToDecimal();
+    }
+
+    // Converti le binaire en int
+    public int binaryToDecimal(){
+        return Integer.parseInt(this.pattern, 2);
+    }
+
+    //
+    public List<Integer> getIndexOfOnes(){
+        List<Integer> indexList = new ArrayList<>();
+        for (int i = 0; i < this.pattern.length(); i++) {
+            if (this.pattern.charAt(i) == '1') {
+                indexList.add(i);
+            }
+        }
+        return indexList;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/fr/esgi/lousvegous/pattern/PatternManager.java
+++ src/main/java/fr/esgi/lousvegous/pattern/PatternManager.java
@@ -1,17 +1,48 @@
 package fr.esgi.lousvegous.pattern;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.SequencedCollection;
 
 public class PatternManager {
-    public static List<Pattern> patterns = new ArrayList<>();
+    public static List<Pattern> patterns = List.of(
+            new Pattern("100 100 100 100 100"), // top line
+            new Pattern("010 010 010 010 010"), // middle line
+            new Pattern("001 001 001 001 001"), // bottom line
+            new Pattern("100 010 001 010 100"), // V
+            new Pattern("001 010 100 010 001"), // reverse V
+            new Pattern("010 100 100 100 010"), // casque diamant
+            new Pattern("001 010 010 010 001"), // middle casque cuir vert
+            new Pattern("010 001 001 001 010"), // bottom cup
+            new Pattern("100 010 010 010 100"), // top cup
+            new Pattern("100 100 010 001 001"), // hélice
+            new Pattern("001 001 010 100 100"), // reverse hélice
+            new Pattern("010 001 010 100 010"), // zig-zag
+            new Pattern("010 100 010 001 010"), // reverse zig-zag
+            new Pattern("100 010 100 010 100"), // top damier
+            new Pattern("001 010 001 010 001"), // bottom damier
+            new Pattern("010 010 100 010 010"), // --|--
+            new Pattern("010 010 001 010 010"), // reverse --|--
+            new Pattern("100 100 001 100 100"), // --_--
+            new Pattern("001 001 100 001 001"), // reverse --_--
+            new Pattern("100 001 001 001 100"), // -___-
+            new Pattern("001 100 100 100 001"), // reverse -___-
+            new Pattern("010 001 100 001 010"), // whither
+            new Pattern("010 100 001 100 010"), // reverse wither
+            new Pattern("100 001 100 001 100"), // -_-_-
+            new Pattern("001 100 001 100 001")  // reverse -_-_-
+    );
 
-    private PatternManager() {
-        // Ajouter les patterns ici
-        addPattern(new Pattern("01101010101111"));
+    public static void main(String[] args) {
+        for (Pattern pattern : patterns) {
+            System.out.println("Pattern: " + pattern.pattern);
+            System.out.println("Indices des 1: " + pattern.getIndexOfOnes());
+        }
     }
 
-    public void addPattern(Pattern pattern) {
-        patterns.add(pattern);
-    }
+//    public void addPattern(Pattern pattern) {
+//        patterns.add(pattern);
+//    }
+
 }
